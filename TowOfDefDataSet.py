4dae699e978c66f33edcffa83c5dffea2a844d95
Nimesh <nimeshjain95@gmail.com>
2017-04-23 17:32:01 -0700

    Update TowOfDefDataSet.py

--- TowOfDefDataSet.py
+++ TowOfDefDataSet.py
@@ -1,6 +1,7 @@
 import os
 from random import randint
 from sklearn.svm import SVC
+
 import cPickle
 import data_parsing
 initialMoney = [200,250,300,350,400]
@@ -22,20 +23,22 @@ def getRow():
     res = []
     money_index = randint(0,4)
     iniMoney = initialMoney[money_index]
-    res.append(iniMoney)
+    #res.append(iniMoney)
 
     final_Money_Rand = randint(0,fin_Money)/100.0
     finalMoney = final_Money_Rand * iniMoney
-    res.append(int(finalMoney))
-
+    #res.append(int(finalMoney))
+    res.append(final_Money_Rand)
     totZombies = randint(totZombies_l,totZombies_h)
     res.append(totZombies)
 
     noOfKilled = (100 - randint(percentage_dead,100))/100.0 * totZombies
-    res.append(int(noOfKilled))
+    #res.append(int(noOfKilled))
+    res.append((100 - randint(percentage_dead,100))/100.0)
 
     noOfKilled_lastSeg = (100 - randint(percentage_dead_last_seg,100))/100.0 * totZombies
-    res.append(int(noOfKilled_lastSeg))
+    #res.append(int(noOfKilled_lastSeg))
+    res.append((100 - randint(percentage_dead_last_seg,100))/100.0)
 
     noOfTowUpgrade = randint(noOfTowerUpgrades_l,noOfTowerUpgrades_h)
     res.append(noOfTowUpgrade)
@@ -50,20 +53,25 @@ def getRow_Low():
     res = []
     money_index = randint(0,4)
     iniMoney = initialMoney[money_index]
-    res.append(iniMoney)
+    #res.append(iniMoney)
 
     final_Money_Rand = (100-randint(0,fin_Money))/100.0
     finalMoney = final_Money_Rand * iniMoney
-    res.append(int(finalMoney))
+    #res.append(int(finalMoney))
+
+    res.append(final_Money_Rand)
+
 
     totZombies = randint(totZombies_l,totZombies_h)
     res.append(totZombies)
 
     noOfKilled = randint(percentage_dead,100)/100.0 * totZombies
-    res.append(int(noOfKilled))
+    #res.append(int(noOfKilled))
+    res.append(randint(percentage_dead,100)/100.0)
 
     noOfKilled_lastSeg = randint(percentage_dead_last_seg,100)/100.0 * totZombies
-    res.append(int(noOfKilled_lastSeg))
+    #res.append(int(noOfKilled_lastSeg))
+    res.append(randint(percentage_dead_last_seg,100)/100.0)
 
     noOfTowUpgrade = randint(0,5)
     res.append(noOfTowUpgrade)
@@ -84,7 +92,7 @@ for i in range(0,500,1):
 '''
 
 def classification():
-    for i in range(0,5000,1):
+    for i in range(0,1000,1):
         row = getRow()
         f_level_index = randint(6,len(fun_level)-1)
         _fun_level = fun_level[f_level_index]
@@ -93,9 +101,12 @@ def classification():
         difficultyLevelOutput.append(_difficulty_level)
         funLevelOuput.append(_fun_level)
         outputMatrix.append(row)
-
-
-    for i in range(0,5000,1):
+    '''
+    with open("output_high.txt","wb") as fid:
+        for i in range(0,len(outputMatrix)-1):
+            fid.write(str(outputMatrix[i]) + ":" + str(funLevelOuput[i]) + ":" + str(difficultyLevelOutput[i]) + "\n")
+    '''
+    for i in range(0,1000,1):
         row = getRow_Low()
         f_level_index = randint(0,5)
         _fun_level = fun_level[f_level_index]
@@ -129,14 +140,28 @@ def classification():
 def evaluate(filename):
     testData = []
     testData.append(data_parsing.getData(filename))
-    print testData
     if not os.path.exists('funlevel_classifier.pkl') or not os.path.exists('difflevel_classifier.pkl'):
         classification()
     with open('funlevel_classifier.pkl', 'rb') as fid:
         clf_fun = cPickle.load(fid)
     with open('difflevel_classifier.pkl', 'rb') as fid:
         clf_diff = cPickle.load(fid)
+    '''
 
+    for i in range(0,50,1):
+        row = getRow()
+        fun_level = clf_fun.predict(row)
+        diff_level = clf_diff.predict(row)
+        print fun_level[0], diff_level[0]
+    print "getting low"
+    for i in range(0,50,1):
+        row = getRow_Low()
+        fun_level = clf_fun.predict(row)
+        diff_level = clf_diff.predict(row)
+        print fun_level[0], diff_level[0]
+
+    print "printing sample jsn"
+    '''
     fun_level = clf_fun.predict(testData)
     diff_level = clf_diff.predict(testData)
     return fun_level[0], diff_level[0]
