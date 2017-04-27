d0c7531d65ec6ba077da3c4979b6b9e5b4b095aa
Bhavya Sanghavi <Bhavya@guest-wireless-207-151-057-177.usc.edu>
2017-04-27 16:45:09 -0700

    Added script to generate training data

---
+++ GenerateTrainingData.py
@@ -0,0 +1,138 @@
+import json
+import os
+event_dict = dict()
+type_dict = dict()
+
+
+initial_Money = 0
+final_Money = 0
+numOfUpgrades = 0
+towerCount = dict()
+monsterKilled = dict()
+monsterKilledLastSeg = dict()
+totZoombies = 0
+def read_game_statistics(filename):
+    json_data = open(filename).read()
+    json_data = json_data.rstrip(',')
+    if json_data[0] != "[":
+        json_data = "[" + json_data + "]"
+
+    data = json.loads(json_data)
+
+    return data
+
+def parse_json(game_data):
+    global totZoombies
+    global initial_Money
+    global final_Money
+    global numOfUpgrades
+    global towerCount
+    global monsterKilled
+    global monsterKilledLastSeg
+    for event in game_data:
+        event_key = event["Event"]
+        type_key = event["Type"]
+        if "Generated" == event_key:
+             totZoombies += event["Num"]
+
+        if event_key not in event_dict:
+            event_dict[event_key] = []
+        event_dict[event_key].append(event)
+
+        if type_key not in type_dict:
+            type_dict[type_key] = []
+        type_dict[type_key].append(event)
+
+    #print event_dict
+    if 'GameStart' in event_dict:
+        initial_Money = event_dict['GameStart'][0]['FinalMoney']
+
+    if 'GameEnd' in event_dict:
+        final_Money = event_dict['GameEnd'][0]['FinalMoney']
+
+    if 'Upgraded' in event_dict:
+        numOfUpgrades = len(event_dict['Upgraded'])
+
+    if 'Built' in event_dict:
+        built_array = event_dict['Built']
+        for b in built_array:
+            key =b['TowerIndex']
+            if key in towerCount:
+                towerCount[key] +=1
+            else:
+                towerCount[key] = 1
+
+    if 'Killed' in event_dict:
+        killed_array = event_dict['Killed']
+        for z in killed_array:
+            if z['MonsterIndex'] in monsterKilled:
+                monsterKilled['MonsterIndex'] += 1
+            else:
+                monsterKilled['MonsterIndex'] = 1
+
+            if z['DistanceLeft'] <= 4:
+                if z['MonsterIndex'] in monsterKilledLastSeg:
+                    monsterKilledLastSeg['MonsterIndex'] += 1
+                else:
+                    monsterKilledLastSeg['MonsterIndex'] = 1
+
+def getData(filename):
+    data = read_game_statistics(filename)
+    parse_json(data)
+    '''
+    print initial_Money
+    print final_Money
+    print numOfUpgrades
+    print(towerCount)
+    print(monsterKilled)
+    print monsterKilledLastSeg
+    print totZoombies
+    '''
+    row = []
+    row.append(initial_Money)
+    row.append(final_Money)
+    row.append(totZoombies)
+    totmonsterkilled = 0
+    totmonsterkilledlastseg = 0
+    totTowerCount = 0
+    for k,v in monsterKilled.iteritems():
+        totmonsterkilled += int(v)
+    for k,v in monsterKilled.iteritems():
+        totmonsterkilledlastseg += int(v)
+    for k,v in towerCount.iteritems():
+        totTowerCount += int(v)
+    row.append(totmonsterkilled)
+    row.append(totmonsterkilledlastseg)
+    row.append(numOfUpgrades)
+    row.append(totTowerCount)
+
+    new_row = []
+
+    new_row.append(float(final_Money) / float(initial_Money))
+    new_row.append(totZoombies)
+    new_row.append(totmonsterkilled/totZoombies)
+    new_row.append(totmonsterkilledlastseg/totZoombies)
+    new_row.append(numOfUpgrades)
+    new_row.append(totTowerCount)
+
+    return new_row
+
+if __name__ == '__main__':
+    textFile = open("labels.txt", "r")
+    trueLabelList = []
+    for trueLabels in textFile.read().split('\n'):
+        trueLabelList.append(trueLabels)
+    count = 0
+    trainingData = open('training.txt', 'w')
+    #f.write('hi there\n')  # python will convert \n to os.linesep
+
+    for filename in os.listdir('log'):
+        if filename[0] != '.':
+            row = getData('log/'+filename)
+            row.append(trueLabelList[count])
+            count += 1
+            for rowItem in row:
+                trainingData.write(str(rowItem)+',')
+            trainingData.write('\n')
+            print(row)
+    trainingData.close()
